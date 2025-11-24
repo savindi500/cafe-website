97a2a936de65ecb96466fa0957883c6ace3064f6
Savindi500 <ssavindi660@gmail.com>
2025-11-24 20:43:23 +0530

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h6 className="text-4xl font-bold text-blue-600">Tailwind is working!</h6>
+    </>
+  );
+}
+
+export default App
